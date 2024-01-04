15cb7f27bb1dc40eac69fcd4ff8eee767898f8cc
James O'Neill <james@jamesoneill.eu>
2024-01-04 15:07:19 +0000

    Add github and linkedin links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,12 @@
+import {
+  IconType,
+  SiGithub,
+  SiGithubHex,
+  SiLinkedin,
+  SiLinkedinHex,
+} from "@icons-pack/react-simple-icons";
 import Link from "next/link";
+import { createElement } from "react";
 
 interface NavLink {
   href: string;
@@ -9,16 +17,43 @@ const links: NavLink[] = [
   {
     href: "/portfolio",
     text: "Portfolio",
-  }, 
+  },
   // {
   //   href: "/contact",
   //   text: "Contact Me",
-  // },  
+  // },
 ];
+
+interface IconLink {
+  altText: string;
+  hoverColor?: string;
+  href: string;
+  icon: IconType;
+}
+
+const iconLinks: IconLink[] = [
+  {
+    href: "https://github.com/James-ONeill",
+    altText: "Visit my GitHub page",
+    hoverColor: SiGithubHex,
+    icon: SiGithub,
+  },
+  {
+    href: "https://github.com/James-ONeill",
+    altText: "Visit my Linkedin page",
+    hoverColor: SiLinkedinHex,
+    icon: SiLinkedin,
+  },
+];
+
+const brandColor = (hex: string) =>
+  ({
+    "--brand-color": hex,
+  }) as React.CSSProperties;
 
 export default function Header() {
   return (
-    <header className="px-8 py-10 border-t-4 border-blue-950">
+    <header className="border-t-4 border-blue-950 px-8 py-10">
       <div className="mx-auto flex max-w-[80rem] items-center justify-between">
         <Link
           href="/"
@@ -27,7 +62,7 @@ export default function Header() {
           James O&apos;Neill
         </Link>
 
-        <nav>
+        <nav className="flex items-center gap-8">
           <ul className="flex gap-8">
             {links.map((link, key) => (
               <li key={key}>
@@ -36,6 +71,23 @@ export default function Header() {
                   className="text-md rounded-md px-4 py-2 text-sm font-semibold uppercase tracking-wide text-blue-950 transition-colors duration-300 hover:bg-gray-200"
                 >
                   {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <ul className="flex gap-4">
+            {iconLinks.map((link, key) => (
+              <li key={key}>
+                <Link
+                  href={link.href}
+                  className="hover:text-brand block transition-colors duration-150"
+                  style={link.hoverColor ? brandColor(link.hoverColor) : {}}
+                >
+                  <span className="sr-only">{link.altText}</span>
+                  {createElement(link.icon, {
+                    className: "h-6 w-6",
+                  })}
                 </Link>
               </li>
             ))}
