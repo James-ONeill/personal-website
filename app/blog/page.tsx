e67c947a15e1d991d92de7aba8d69f33e6434418
James O'Neill <james@jamesoneill.eu>
2024-02-19 15:06:05 +0000

    Set publication status on posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,39 +1,8 @@
 import Container from "@/components/Container";
-import fs from "fs";
-import matter from "gray-matter";
 import type { Metadata } from "next";
 import Link from "next/link";
-import path from "path";
-
-interface PostMetadata {
-  id: string;
-  date: string;
-  title: string;
-}
-
-const postsDirectory = path.join(process.cwd(), "app/blog/(posts)");
-
-function getSortedPostsData() {
-  return fs
-    .readdirSync(postsDirectory)
-    .filter(
-      (name) =>
-        fs.lstatSync(path.join(postsDirectory, name)).isDirectory() &&
-        fs.lstatSync(path.join(postsDirectory, name, "page.mdx")).isFile(),
-    )
-    .map((id): PostMetadata => {
-      const fullPath = path.join(postsDirectory, id, "page.mdx");
-      const fileContents = fs.readFileSync(fullPath, "utf8");
-      const { data } = matter(fileContents);
-
-      return {
-        id,
-        date: data.date,
-        title: data.title,
-      };
-    })
-    .sort((a, b) => (a.date < b.date ? 1 : -1));
-}
+import { getSortedPostsData } from "./posts";
+import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Blog | James O'Neill",
@@ -41,6 +10,8 @@ export const metadata: Metadata = {
 
 export default function Blog() {
   const posts = getSortedPostsData();
+
+  if (posts.length < 1) return notFound();
 
   return (
     <main>
